374c5cf7c31f8d71c5433128bc4423e15e55616b
nesterukm <nesterukm@wp.pl>
2019-04-23 18:49:11 +0200

    23_petle

--- Pierwszy/Program.cs
+++ Pierwszy/Program.cs
@@ -10,7 +10,28 @@ namespace Pierwszy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cześć");
+            //string name = args[0];
+
+            Console.Write("Cześć jak ma sz imię: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Cześć "+name);
+
+            Console.Write("Podaj wiek: ");
+
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Jesteś pełnoletni!");
+            }
+            else
+            {
+                Console.WriteLine("Gnojek");
+            }
+            Console.WriteLine("");
+
+            Console.ReadLine();
+            
 
         }
     }
