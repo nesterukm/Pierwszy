f1111988aa668a908d95de7ff399d75dcfdf2ee5
Nesti <nerexo@wp.pl>
2019-04-24 12:10:32 +0200

    sdfsdfs

--- Pierwszy/Program.cs
+++ Pierwszy/Program.cs
@@ -10,29 +10,13 @@ namespace Pierwszy
     {
         static void Main(string[] args)
         {
-            //string name = args[0];
+            Console.WriteLine("Cześć");
 
-            Console.Write("Cześć jak ma sz imię: ");
+        private static void Greeting()
+        {
+            Console.Write("Podaj Imię:");
             string name = Console.ReadLine();
-            Console.WriteLine("Cześć "+name);
-
-            Console.Write("Podaj wiek: ");
-
-            int age = int.Parse(Console.ReadLine());
-
-            if (age >= 18)
-            {
-                Console.WriteLine("Jesteś pełnoletni!");
-            }
-            else
-            {
-                Console.WriteLine("Gnojek");
-            }
-            Console.WriteLine("");
-
-            Console.ReadLine();
-            
-
+            Console.WriteLine("Cześć " + name);
         }
     }
 }
